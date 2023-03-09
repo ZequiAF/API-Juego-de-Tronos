fba35174ca8e83e628da1e7c31a1aac42f4bd8ba
Andresito <alumno.577931@ies-azarquiel.es>
2023-03-09 11:51:58 +0100

    NAVOS COMENTADOS 🥖

--- personajes.js
+++ personajes.js
@@ -2,6 +2,7 @@ import localforage from "localforage";
 import { matchSorter } from "match-sorter";
 import sortBy from "sort-by";
 
+// Listar todos los personajes con sus frases
 export async function getCharactersAndQuotes(params) {
   let personajes = personajes.find(contact => contact.params === params);
   if (!personajes) personajes = [];
@@ -11,6 +12,7 @@ export async function getCharactersAndQuotes(params) {
   return personajes.sort(sortBy("last", "createdAt"));
 }
 
+// Mostrar información de 1 personaje de la serie
 export async function getCharactersByName(params) {
     let personaje = await localforage.getItem("personaje");
     let contact = personaje.find(contact => contact.params === params);
